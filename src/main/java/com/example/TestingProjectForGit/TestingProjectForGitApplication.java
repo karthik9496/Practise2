a81376f78348557edab4fc2dc548e7f1dde15b75
karthik <karthik.velishala54@gmail.com>
2024-02-08 22:10:56 +0530

    First commit

---
+++ src/main/java/com/example/TestingProjectForGit/TestingProjectForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestingProjectForGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingProjectForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingProjectForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestingProjectForGit/TestingProjectForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingProjectForGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingProjectForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
